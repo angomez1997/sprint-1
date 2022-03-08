5320b62601592e0c0d849f84eb701ce6edbfe31e
Angie Gomez <angiegomez0706@gmail.com>
2022-03-07 23:39:01 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const capturarDatos =() =>{
+
+    let cantidad= document.getElemementByid("lista-carrito").value;
+    let boton=  document.getElemementByid("button")
+    let precio = document.getElementById("calcular")
+    let total = cantidad * precio
+
+    if()
+}
